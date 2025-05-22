5143e62b27fe8877709fb5f954a1d9193ebd87aa
Neo Ighodaro <neo.ighodaro@pinktum.com>
2025-05-22 07:52:40 +0200

    Initial commit.

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,4 +3,11 @@ interface BoopState {
   intent: BoopIntents;
 }
 
+interface ActionsProps {
+  handleSubmit: (state: BoopState) => void;
+  onStart?: (state: BoopState) => void;
+  onFailure?: (state: BoopState) => void;
+  onSuccess?: (state: BoopState) => void;
+}
+
 type BoopIntents = "trimWhitespaces";
